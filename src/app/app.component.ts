15c0d82ec5cf4bca0d3ac71e4f53032565457226
shenghaoc <shenghaoc@outlook.com>
2024-01-14 23:14:27 +0800

    Remove CommonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { PredictionToolComponent } from './prediction-tool/prediction-tool.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PredictionToolComponent],
+  imports: [RouterOutlet, PredictionToolComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
