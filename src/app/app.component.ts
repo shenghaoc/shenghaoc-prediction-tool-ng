a09fe4efa4fb72ede7d914ba6ef94014c1c24c04
shenghaoc <shenghaoc@outlook.com>
2024-01-14 12:46:14 +0800

    Migrate project to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PredictionToolComponent } from './prediction-tool/prediction-tool.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [PredictionToolComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'prediction-tool-ng';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideClientHydration(),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
