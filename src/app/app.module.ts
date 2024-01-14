f124528cb8c0ee0cf0ef40b98ee555a2c7250a2e
shenghaoc <shenghaoc@outlook.com>
2024-01-14 11:08:48 +0800

    Revert "Add select menu"
    This reverts commit 1939d282b59c67fccb40fd50ebf664438e5207ad.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MlModelSelectComponent } from './ml-model-select/ml-model-select.component';
 import { PredictionToolComponent } from './prediction-tool/prediction-tool.component';
-
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MlModelSelectComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatSelectModule,
     PredictionToolComponent
   ],
   providers: [
