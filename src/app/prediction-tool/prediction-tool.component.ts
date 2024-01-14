34e95c6a97cf6780e82da93cbf6771c46fe969b8
shenghaoc <shenghaoc@outlook.com>
2024-01-14 23:15:57 +0800

    Add dummy chart

--- src/app/prediction-tool/prediction-tool.component.ts
+++ src/app/prediction-tool/prediction-tool.component.ts
@@ -7,6 +7,10 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 import { ml_model_list } from '../lists';
 import { town_list } from '../lists';
@@ -16,7 +20,7 @@ import { flat_model_list } from '../lists';
 @Component({
   selector: 'app-prediction-tool',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule, NgChartsModule],
   templateUrl: './prediction-tool.component.html',
   styleUrl: './prediction-tool.component.css'
 })
@@ -43,8 +47,31 @@ export class PredictionToolComponent {
     console.warn(this.predictionForm.value);
   }
 
-  constructor(private formBuilder: FormBuilder) {
+
+  isBrowser: boolean;
+  constructor(private formBuilder: FormBuilder, @Inject(PLATFORM_ID) platformId: string) {
     this.minDate = new Date("1960-01-01");
     this.maxDate = new Date("2022-02-01");
+    this.isBrowser = isPlatformBrowser(platformId);
   }
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+        display: false,
+        text: 'Predicted Trends for Past 12 Months'
+      }
+    }
+  };
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
 }
